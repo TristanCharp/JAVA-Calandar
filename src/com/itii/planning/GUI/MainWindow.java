358b2f7a96afcdbc886e14f90a727996bc24be76
tristan_charp <tristan.charpentier06@gmail.com>
2019-04-27 16:03:53 +0200

    Class and package creation

---
+++ src/com/itii/planning/GUI/MainWindow.java
@@ -0,0 +1,17 @@
+package com.itii.planning.GUI;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    private JPanel panel1;
+    private JButton button1;
+
+    
+
+    private MainWindow(){
+        initialize();
+    }
+    private void initialize(){
+
+    }
+}
--- src/com/itii/planning/Launcher.java
+++ src/com/itii/planning/Launcher.java
@@ -1,4 +1,7 @@
 package com.itii.planning;
 
 public class Launcher {
+    public static void main(String[] args) {
+
+    }
 }
