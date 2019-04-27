770b755dcd552c921464e31a251cd08b8b887fe8
tristan_charp <tristan.charpentier06@gmail.com>
2019-04-27 18:00:17 +0200

    no message

--- src/com/itii/planning/GUI/MainWindow.java
+++ src/com/itii/planning/GUI/MainWindow.java
@@ -1,11 +1,31 @@
+/*
+ * Created by JFormDesigner on Sat Apr 27 16:53:58 CEST 2019
+ */
+
 package com.itii.planning.GUI;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
+
     private JPanel panel1;
-    private JButton button1;
     private static MainWindow mainWindow;
+
+
+    private MainWindow() {
+        initialize();
+    }
+
+
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - tristan
+    private JMenuBar menuBar1;
+    private JMenu menu1;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+
+
 
     public static MainWindow getMainWindow() {
         if ( mainWindow == null){
@@ -14,15 +34,52 @@ public class MainWindow extends JFrame {
         return mainWindow;
     }
 
-    private MainWindow(){
-        initialize();
-    }
     private void initialize(){
         setSize(640,480);
         setVisible(true);
 
+
         this.validate();
         this.repaint();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - tristan
+        menuBar1 = new JMenuBar();
+        menu1 = new JMenu();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //======== menuBar1 ========
+        {
+
+            //======== menu1 ========
+            {
+                menu1.setText("FIle");
+            }
+            menuBar1.add(menu1);
+        }
+        setJMenuBar(menuBar1);
+
+        { // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 }
