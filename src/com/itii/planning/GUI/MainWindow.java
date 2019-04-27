911adf7b43767a31a8e39d4ed1966642a2316549
Tristan Charpentier <tristan.charpentier06@gmail.com>
2019-04-27 18:11:35 +0200

    Add all menu bar and sub menu

--- src/com/itii/planning/GUI/MainWindow.java
+++ src/com/itii/planning/GUI/MainWindow.java
@@ -24,6 +24,18 @@ public class MainWindow extends JFrame {
     // Generated using JFormDesigner Evaluation license - tristan
     private JMenuBar menuBar1;
     private JMenu menu1;
+    private JMenuItem menuItem1;
+    private JMenu menu2;
+    private JMenuItem menuItem2;
+    private JMenuItem menuItem3;
+    private JMenuItem menuItem4;
+    private JMenuItem menuItem5;
+    private JMenuItem menuItem6;
+    private JMenu menu3;
+    private JMenuItem menuItem7;
+    private JMenuItem menuItem8;
+    private JMenuItem menuItem9;
+    private JMenu menu4;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 
 
@@ -50,6 +62,18 @@ public class MainWindow extends JFrame {
         // Generated using JFormDesigner Evaluation license - tristan
         menuBar1 = new JMenuBar();
         menu1 = new JMenu();
+        menuItem1 = new JMenuItem();
+        menu2 = new JMenu();
+        menuItem2 = new JMenuItem();
+        menuItem3 = new JMenuItem();
+        menuItem4 = new JMenuItem();
+        menuItem5 = new JMenuItem();
+        menuItem6 = new JMenuItem();
+        menu3 = new JMenu();
+        menuItem7 = new JMenuItem();
+        menuItem8 = new JMenuItem();
+        menuItem9 = new JMenuItem();
+        menu4 = new JMenu();
 
         //======== this ========
         Container contentPane = getContentPane();
@@ -60,9 +84,63 @@ public class MainWindow extends JFrame {
 
             //======== menu1 ========
             {
-                menu1.setText("File");
+                menu1.setText("Fichier");
+
+                //---- menuItem1 ----
+                menuItem1.setText("Quitter");
+                menu1.add(menuItem1);
             }
             menuBar1.add(menu1);
+
+            //======== menu2 ========
+            {
+                menu2.setText("Edition");
+
+                //---- menuItem2 ----
+                menuItem2.setText("Cr\u00e9er");
+                menu2.add(menuItem2);
+
+                //---- menuItem3 ----
+                menuItem3.setText("Editer");
+                menu2.add(menuItem3);
+
+                //---- menuItem4 ----
+                menuItem4.setText("Marquer");
+                menu2.add(menuItem4);
+
+                //---- menuItem5 ----
+                menuItem5.setText("Dupliquer");
+                menu2.add(menuItem5);
+
+                //---- menuItem6 ----
+                menuItem6.setText("Supprimer");
+                menu2.add(menuItem6);
+            }
+            menuBar1.add(menu2);
+
+            //======== menu3 ========
+            {
+                menu3.setText("Vue");
+
+                //---- menuItem7 ----
+                menuItem7.setText("Liste");
+                menu3.add(menuItem7);
+
+                //---- menuItem8 ----
+                menuItem8.setText("Semaine");
+                menu3.add(menuItem8);
+
+                //---- menuItem9 ----
+                menuItem9.setText("Mois");
+                menu3.add(menuItem9);
+            }
+            menuBar1.add(menu3);
+
+            //======== menu4 ========
+            {
+                menu4.setText("Aide");
+            }
+            menuBar1.add(menu4);
         }
         setJMenuBar(menuBar1);
 
