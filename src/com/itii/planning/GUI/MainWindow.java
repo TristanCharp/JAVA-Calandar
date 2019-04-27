bb8f5aad87e2f1623a68db2930fa07827a8cdd11
tristan_charp <tristan.charpentier06@gmail.com>
2019-04-27 16:20:16 +0200

    no message

--- src/com/itii/planning/GUI/MainWindow.java
+++ src/com/itii/planning/GUI/MainWindow.java
@@ -5,13 +5,20 @@ import javax.swing.*;
 public class MainWindow extends JFrame {
     private JPanel panel1;
     private JButton button1;
+    private MainWindow mainWindow;
 
-    
+    public MainWindow getMainWindow() {
+        if ( mainWindow == null){
+            mainWindow = new MainWindow();
+        }
+        return mainWindow;
+    }
 
     private MainWindow(){
         initialize();
     }
     private void initialize(){
-
+        setSize(50,50);
+        setVisible(true);
     }
 }
