033875ffcf3f5b897e2d4bba9a9a810ba1315487
tristan_charp <tristan.charpentier06@gmail.com>
2019-05-11 15:43:53 +0200

    no message

--- src/com/itii/planning/GUI/MainWindow.java
+++ src/com/itii/planning/GUI/MainWindow.java
@@ -2,6 +2,11 @@ package com.itii.planning.GUI;
 
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
 
 public class MainWindow extends JFrame {
 
@@ -31,14 +36,50 @@ public class MainWindow extends JFrame {
         this.validate();
         this.repaint();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
         
-        JPanel panel = new JPanel();
-        getContentPane().add(panel, BorderLayout.NORTH);
+        JPanel panel_1 = new JPanel();
+        panel_1.setBounds(501, 79, 123, 224);
+        getContentPane().add(panel_1);
+        panel_1.setLayout(null);
+        
+        JButton btnNewButton_1 = new JButton("Creer");
+        btnNewButton_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        btnNewButton_1.setBounds(0, 11, 89, 23);
+        panel_1.add(btnNewButton_1);
+        
+        JButton btnNewButton = new JButton("Editer");
+        btnNewButton.setBounds(0, 45, 89, 23);
+        panel_1.add(btnNewButton);
+        
+        JButton btnNewButton_2 = new JButton("Marquer");
+        btnNewButton_2.setBounds(0, 79, 89, 23);
+        panel_1.add(btnNewButton_2);
+        
+        JButton btnNewButton_3 = new JButton("Dupliquer");
+        btnNewButton_3.setBounds(0, 113, 89, 23);
+        panel_1.add(btnNewButton_3);
+        
+        JButton btnSupprimer = new JButton("Supprimer");
+        btnSupprimer.setBounds(0, 147, 89, 23);
+        panel_1.add(btnSupprimer);
         
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         
-        JMenuItem mntmNewMenuItem = new JMenuItem("Fichier");
+        JMenuItem mntmNewMenuItem_2 = new JMenuItem("Fichier");
+        menuBar.add(mntmNewMenuItem_2);
+        
+        JMenuItem mntmNewMenuItem_1 = new JMenuItem("Edition");
+        menuBar.add(mntmNewMenuItem_1);
+        
+        JMenuItem mntmNewMenuItem = new JMenuItem("Vue");
         menuBar.add(mntmNewMenuItem);
+        
+        JMenuItem mntmNewMenuItem_3 = new JMenuItem("Aide");
+        menuBar.add(mntmNewMenuItem_3);
     }
 }
